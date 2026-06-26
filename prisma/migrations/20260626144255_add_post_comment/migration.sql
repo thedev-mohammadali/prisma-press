4c67850b56e4975906e83608d7079ee4c98cda1c
Mohammad Ali <thedev.mohammadali@gmail.com>
2026-06-26 20:43:29 +0600

    feat(prisma): add user and post model

---
+++ prisma/migrations/20260626144255_add_post_comment/migration.sql
@@ -0,0 +1,53 @@
+-- CreateEnum
+CREATE TYPE "PostStatus" AS ENUM ('DRAFT', 'PUBLISHED', 'ARCHIVED');
+
+-- CreateEnum
+CREATE TYPE "CommentStatus" AS ENUM ('APPROVED', 'REJECTED');
+
+-- CreateTable
+CREATE TABLE "comments" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "author_id" TEXT NOT NULL,
+    "post_id" TEXT NOT NULL,
+    "status" "CommentStatus" NOT NULL DEFAULT 'APPROVED',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT NOT NULL,
+    "thumbnail" TEXT,
+    "is_featured" BOOLEAN NOT NULL DEFAULT false,
+    "status" "PostStatus" NOT NULL DEFAULT 'PUBLISHED',
+    "tags" TEXT[],
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "authro_id" TEXT NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "comments_post_id_idx" ON "comments"("post_id");
+
+-- CreateIndex
+CREATE INDEX "comments_author_id_idx" ON "comments"("author_id");
+
+-- CreateIndex
+CREATE INDEX "posts_authro_id_idx" ON "posts"("authro_id");
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_authro_id_fkey" FOREIGN KEY ("authro_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
